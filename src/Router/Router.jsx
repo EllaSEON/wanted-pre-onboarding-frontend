5cff810ace8272f3572f6a745de03f61c805f605
EllaSEON <shineunhye5@gmail.com>
2023-02-13 16:14:47 +0900

    <feat> : NotFound 페이지 생성

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -5,6 +5,7 @@ import Splash from "../pages/Splash/Splash";
 import SignUp from "../pages/SignUp/SignUp";
 import SignIn from "../pages/SignIn/SingIn";
 import ToDo from "../pages/ToDo/ToDo";
+import NotFound from "../pages/NotFound/NotFound";
 
 const Router = () => {
   const { user } = useContext(AuthContext);
@@ -24,6 +25,7 @@ const Router = () => {
             <Route path="/" element={<Splash />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/signin" element={<SignIn />} />
+            <Route path="/*" element={<NotFound />} />
           </Route>
         )}
       </Routes>
